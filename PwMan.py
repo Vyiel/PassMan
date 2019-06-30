01e7fc4089a9afa71cab1996ac38c11abb6fa5f0
$ git config --global user.name John <budhaditya.blog@gmail.com>
2019-06-30 16:13:52 +0530

    First commit. Basic classes has been just created Problem with OOPS understanding. Keeping the project in Hold until I comeup with a fix. All the basic functions and classes are working till now somehow
    Latest commit : Emergency computer re - configuration

--- PwMan.py
+++ PwMan.py
@@ -1,12 +1,13 @@
 import sqlite3
 from selenium import webdriver
 import time
+import sys
 
 listall = {}
 id = []
 name = []
 
-class db_methods:
+class Db_methods:
 
 
     def __init__(self):
@@ -23,15 +24,12 @@ class db_methods:
         self.conn.commit()
 
     def save(self, uname, passw, service):
-        self.uname = uname
-        self.passw = passw
-        self.service = service
+
         try:
             self.c.execute("INSERT INTO userinfo (uname, passw, service) VALUES (?, ?, ?)",
-                           (self.uname, self.passw, self.service))
+                           (uname, passw, service))
             self.conn.commit()
-            print "Information for " + self.uname + " Saved to Database successfully"
-            print db.read_all()
+            print("Information for " + self.uname + " Saved to Database successfully")
             main()
         except:
             "Couldn't save to Database!!!"
@@ -42,54 +40,56 @@ class db_methods:
         return self.c.fetchall()
 
     def read_one(self, id):
-        self.id = id
-        self.c.execute("SELECT * FROM userinfo WHERE ID = ?", [self.id])
+        self.c.execute("SELECT * FROM userinfo WHERE ID = ?", [id])
         return self.c.fetchone()
 
     def update(self, uname, passw, service, id):
-        self.uname = uname
-        self.passw = passw
-        self.service = service
-        self.id = id
+
         try:
             self.c.execute \
                 ("""
             UPDATE userinfo SET uname=?, passw=?, service=? WHERE ID = ?"""
-                 , (self.uname, self.passw, self.service, self.id))
+                 , (uname, passw, service, id))
             self.conn.commit()
-            print "Credentials updated successfully"
+            print("Credentials updated successfully")
             main()
         except:
             "Couldn't Update Database!!!"
             main()
 
     def remove(self, id):
-        self.id = id
         try:
-            self.c.execute("DELETE FROM userinfo WHERE ID = ?", [self.id])
+            self.c.execute("DELETE FROM userinfo WHERE ID = ?", [id])
             self.conn.commit()
-            print "Credentials successfully removed"
-            print db.read_all()
+            print("Credentials successfully removed")
             main()
         except:
-            print "Couldn't delete information from Database!!!"
+            print("Couldn't delete information from Database!!!")
             main()
 
 
-class login:
+class Login:
     def google(self, uname, passw):
         self.uname = uname
         self.passw = passw
-        driver = webdriver.Chrome('C:\chromedriver')
-        driver.get(
-            'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+        chromedriver_location = 'C:\chromedriver'
+        try:
+            driver = webdriver.Chrome(chromedriver_location)
+        except:
+            print("""Probably The chrome driver is not matched with your current one.
+            Please update chrome to the latest version, and download and save the latest
+            stable version of chromedriver on to C:\\ """)
+
+        target = 'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
+        driver.get(target)
+
         driver.find_element_by_id("identifierId").send_keys(self.uname)
         driver.find_element_by_class_name('CwaK9').click()
         time.sleep(2)
         driver.find_element_by_name("password").send_keys(passw)
         driver.find_element_by_class_name("CwaK9").click()
-        driver.close()
-        main()
+        # driver.close()
+        # main()
 
     def facebook(self, uname, passw):
         self.uname = uname
@@ -99,21 +99,22 @@ class login:
         time.sleep(2)
         driver.find_element_by_id('email').send_keys(self.uname)
         driver.find_element_by_id('pass').send_keys(self.passw)
-        driver.find_element_by_id('u_0_a').click()
+        driver.find_element_by_id('login_form').submit()
         driver.close()
         main()
 
 
-class basic:
-    found = False
+class Basic:
     listall = {}
     list_of_id = []
 
     def __init__(self):
-        listInfo = db.read_all()
+        listall.clear()
+        self.db = Db_methods()
+        self.lg = Login()
+        listInfo = self.db.read_all()
         if len(listInfo) == 0:
-            print "There is no information in the database. Please add logins first!!! "
-            main()
+            print("There is no information in the database. Please add logins first!!! ")
         else:
             for i in listInfo:
                 id = int(i[0])
@@ -121,22 +122,16 @@ class basic:
                 service = str(i[3])
                 listall[id] = name, service
 
-
-
-
-    def check_id(self, id, list_of_id):
-        self.id = id
-        self.list_of_id = list_of_id
-
-        if self.id in self.list_of_id:
+    @staticmethod
+    def check_id(id, list_of_id):
+        if id in list_of_id:
             return True
         else:
             return False
-
     @staticmethod
     def display():
 
-        print "--> All Accounts <--"
+        print("--> All Accounts <--")
 
         for items in listall:
             if listall[items][1] == "ggl":
@@ -144,148 +139,117 @@ class basic:
             elif listall[items][1] == "fb":
                 service = "Facebook"
 
-            print (str(items) +"     "+ listall[items][0]+"     "+ service)
-        print
+            print(str(items) +"     "+ listall[items][0]+"     "+ service)
+        print("")
 
 
-    @staticmethod
-    def add():
+    def add(self):
         while True:
-            uname = raw_input('Enter Username: ')
-            passw = raw_input('Enter Password: ')
-            service = raw_input('Enter Service -> ggl for "Google" and fb for "Facebook" : ')
+            uname = input('Enter Username: ')
+            passw = input('Enter Password: ')
+            service = input('Enter Service -> ggl for "Google" and fb for "Facebook" : ')
             if service == "ggl" or service == "fb":
-                db.save(uname, passw, service)
+                self.db.save(uname, passw, service)
                 break
             else:
-                print ("Incorrect Service Name!")
+                print("Incorrect Service Name!")
                 continue
 
 
 
-    @staticmethod
-    def update():
-        print "--> Update Accounts <--"
-        for items in listall:
-            if listall[items][1] == "ggl":
-                service = "Google"
-            elif listall[items][1] == "fb":
-                service = "Facebook"
-
-            print (str(items) + "     " + listall[items][0] + "     " + service)
-        print
+    def update(self):
+        self.display()
 
         while True:
             try:
                 select = int(input("Enter choice of ID from before the User Names to Update: "))
                 break
             except:
-                print "selection type for update not integer!!! "
+                print("selection type for update not integer!!! ")
                 continue
 
         list_of_id = listall.keys()
-        opt = bf.check_id(select, list_of_id)
+        opt = self.check_id(select, list_of_id)
         if opt is True:
-            uname = raw_input('Enter Username to update: ')
-            passw = raw_input('Enter Password to update: ')
+            uname = input('Enter Username to update: ')
+            passw = input('Enter Password to update: ')
             while True:
-                service = raw_input('Enter Service initials: ex:- "ggl/fb": ')
+                service = input('Enter Service initials: ex:- "ggl/fb": ')
                 if service == "ggl" or service == "fb":
-                    db.update(uname, passw, service, select)
+                    self.db.update(uname, passw, service, select)
                     break
                 else:
-                    print "Service Initials are not correct!!! "
+                    print("Service Initials are not correct!!! ")
                     continue
         else:
-            print "User information for this ID not found in the Database"
+            print("User information for this ID not found in the Database")
 
-    @staticmethod
-    def delete():
-        print "--> Delete Accounts <--"
-        for items in listall:
-            if listall[items][1] == "ggl":
-                service = "Google"
-            elif listall[items][1] == "fb":
-                service = "Facebook"
-
-            print (str(items) + "     " + listall[items][0] + "     " + service)
-        print
+    def delete(self):
+        print("--> Remove Accounts <--")
+        self.display()
 
         while True:
             try:
                 select = int(input("Enter choice of ID from before the User Names to Delete: "))
                 break
             except:
-                print "selection type for deletion not integer!!! "
+                print("selection type for deletion not integer!!! ")
                 continue
 
         list_of_id = listall.keys()
-        opt = bf.check_id(select, list_of_id)
-
+        opt = self.check_id(select, list_of_id)
         if opt is True:
-            db.remove(select)
+            self.db.remove(select)
         else:
-            print "User information for this ID not found in the Database"
-            main()
+            print("User information for this ID not found in the Database")
 
 
-    @staticmethod
-    def login():
-        print "--> Login <--"
-        for items in listall:
-            if listall[items][1] == "ggl":
-                service = "Google"
-            elif listall[items][1] == "fb":
-                service = "Facebook"
-
-            print (str(items) + "     " + listall[items][0] + "     " + service)
-        print
+    def login(self):
+        print("--> Login <--")
+        self.display()
 
         while True:
             try:
                 select = int(input("Enter choice of ID from before the User Names to Login: "))
                 break
             except:
-                print "selection type for deletion not integer!!! "
+                print("selection type for deletion not integer!!! ")
                 continue
 
         list_of_id = listall.keys()
-        opt = bf.check_id(select, list_of_id)
+        opt = self.check_id(select, list_of_id)
         if opt is True:
-            creds = db.read_one(select)
+            creds = self.db.read_one(select)
             uname = creds[1]
             passw = creds[2]
             service = creds[3]
             if service == "ggl":
-                lg.google(uname, passw)
+                self.lg.google(uname, passw)
             elif service == "fb":
-                lg.facebook(uname, passw)
+                self.lg.facebook(uname, passw)
             else:
-                print "Invalid Service!!!"
+                print("Invalid Service!!!")
         else:
-            print "User information for this ID not found in the Database"
+            print("User information for this ID not found in the Database")
             main()
 
 
-db = db_methods()
-bf = basic()
-lg = login()
 
 def main():
-    basic()
+    bf = Basic()
     sdict_select = int()
     sdict_res = str()
 
-    print """
+    print("""
         --> Choice of Options <--
-        
+
         1 -> Display Accounts
         2 -> Add User Information
         3 -> Update User Information
         4 -> Delete User Information
         5 -> Login with Credentials
-    
-        """
+
+        """)
 
     sdict = {
         1: bf.display,
@@ -303,11 +267,19 @@ def main():
             break
 
         except:
-            print ("Invalid Selection!!! ")
+            print("Invalid Selection!!! ")
             continue
 
 if __name__ == '__main__':
-    main()
+    while True:
+        try:
+            main()
+        except KeyboardInterrupt:
+            print('Process is killed by keyboard interrupt')
+            time.sleep(5)
+            sys.exit(0)
+
+
 
 
 
@@ -316,9 +288,9 @@ if __name__ == '__main__':
 
 # TEST RUN FOR INDIVIDUAL DATABASE FUNCTIONS #
 # db.save('whatever u', 'whatever p', 'ggl') # WORKS FINE #
-# print db.read_all() # WORKS FINE #
-# print db.read_one(2) # WORKS FINE #
-# db.update('what', 'what', 'fb', 3) # WORKS FINE #
+# print(db.read_all()) # WORKS FINE #
+# print(db.read_one(1)) # WORKS FINE #
+# db.update('what', 'what', 'fb', 1) # WORKS FINE #
 # db.remove(3) # WORKS FINE #
 
 # TEST RUN FOR INDIVIDUAL BASIC FUNCTIONS #
@@ -327,3 +299,8 @@ if __name__ == '__main__':
 # bf.login() # WORKS FINE #
 # bf.delete() # WORKS FINE #
 # bf.display() # WORKS FINE #
+# bf = Basic() Works Fine
+
+# list_of_id = listall.keys()
+# a = bf.check_id(2, list_of_id)
+# print(a)
