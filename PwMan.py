96f9f8fe5d733214886be7b8fd22014ec245fd65
Budhaditya Bose <budhaditya.blog@gmail.com>
2019-07-27 02:51:36 +0530

    Tried to implement Password Encrpytion. Just working on the Hashing mechanism now. Commiting for emergency issues.
    Password Encryption implemented. Hashing, Encryption with Block Chain mode, And all the basic functionalities are over with. Now need to over come a lot of security issues, File encryption, Database Encryption, Argparse etc,
    Threading done for session timeout aswell.
    Committing last working condition

--- PwMan.py
+++ PwMan.py
@@ -3,12 +3,16 @@ from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto import Random
 from selenium import webdriver
-import time
 import sys
+import os
+import time
+from threading import Thread
 
 listall = {}
 id = []
 name = []
+global _session
+_session = False
 
 class Db_methods:
 
@@ -32,6 +36,16 @@ class Db_methods:
                 (
                     ID INTEGER PRIMARY KEY,
                     master_pass TEXT
+                )
+                """)
+        self.conn.commit()
+
+        self.conn = sqlite3.connect('db')
+        self.c = self.conn.cursor()
+        self.c.execute("""CREATE TABLE IF NOT EXISTS vector
+                (
+                    ID INTEGER PRIMARY KEY,
+                    IV NONE
                 )
                 """)
         self.conn.commit()
@@ -81,7 +95,6 @@ class Db_methods:
             main()
 
     def save_pass(self, master_pass):
-        global id
         id = 1
         try:
             self.c.execute("INSERT INTO secret (id, master_pass) VALUES (?, ?)", (id, master_pass))
@@ -91,6 +104,7 @@ class Db_methods:
         except:
             print("Couldn't save to Database!!!")
             main()
+
 
     def update_pass(self, master_pass):
         id = 1
@@ -120,7 +134,39 @@ class Db_methods:
             print("Couldn't remove password from Database!!!")
             main()
 
+    def save_vector(self, IV):
+        id = 1
+        try:
+            self.c.execute("INSERT INTO vector (ID, IV) VALUES (?, ?)", (id, IV))
+            self.conn.commit()
+            print("IV saved to database")
+        except sqlite3.Error as e:
+            print(e)
+            print("Couldn't write vector to DB")
 
+    def update_vector(self, IV):
+        id = 1
+        try:
+            self.c.execute("UPDATE vector SET IV=? WHERE ID=?", (IV, id))
+            self.conn.commit()
+        except:
+            print("Couldn't update vector to DB")
+
+    def retrieve_vector(self):
+        id = 1
+        try:
+            self.c.execute("SELECT IV FROM vector WHERE ID = ?", [id])
+            return self.c.fetchone()
+        except sqlite3.Error as e:
+            print(e)
+
+    def remove_vector(self):
+        id = 1
+        try:
+            self.c.execute("DELETE FROM vector WHERE ID = ?", [id])
+            self.conn.commit()
+        except:
+            print("Couldn't remove vector from Database!!!")
 
 
 class Login:
@@ -130,26 +176,25 @@ class Login:
         self.uname = uname
         self.passw = passw
 
-        print(self.uname, self.passw)
 
-        # chromedriver_location = 'C:\chromedriver'
-        # try:
-        #     driver = webdriver.Chrome(chromedriver_location)
-        # except:
-        #     print("""Probably The chrome driver is not matched with your current one.
-        #     Please update chrome to the latest version, and download and save the latest
-        #     stable version of chromedriver on to C:\\ """)
-        #
-        # target = 'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
-        # driver.get(target)
-        #
-        # driver.find_element_by_id("identifierId").send_keys(self.uname)
-        # driver.find_element_by_class_name('CwaK9').click()
-        # time.sleep(2)
-        # driver.find_element_by_name("password").send_keys(passw)
-        # driver.find_element_by_class_name("CwaK9").click()
-        # driver.close()
-        # main()
+        chromedriver_location = 'C:\chromedriver'
+        try:
+            driver = webdriver.Chrome(chromedriver_location)
+        except:
+            print("""Probably The chrome driver is not matched with your current one.
+            Please update chrome to the latest version, and download and save the latest
+            stable version of chromedriver on to C:\\ """)
+
+        target = 'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
+        driver.get(target)
+
+        driver.find_element_by_id("identifierId").send_keys(self.uname)
+        driver.find_element_by_class_name('CwaK9').click()
+        time.sleep(2)
+        driver.find_element_by_name("password").send_keys(passw)
+        driver.find_element_by_class_name("CwaK9").click()
+        driver.close()
+        main()
 
     def facebook(self, uname, passw):
         self.uname = uname
@@ -210,7 +255,7 @@ class Helper:
             passw = input('Enter Password: ')
             service = input('Enter Service -> ggl for "Google" and fb for "Facebook" : ')
             if service == "ggl" or service == "fb":
-                enc_pass = self.sec.Encrypt(passw, self.check_pass())
+                enc_pass = self.sec.Encrypt(passw, self.check_pass(), self.IV())
                 self.db.save(uname, enc_pass, service)
                 break
             else:
@@ -237,7 +282,7 @@ class Helper:
             while True:
                 service = input('Enter Service initials: ex:- "ggl/fb": ')
                 if service == "ggl" or service == "fb":
-                    enc_pass = self.sec.Encrypt(passw, self.check_pass())
+                    enc_pass = self.sec.Encrypt(passw, self.check_pass(), self.IV())
                     self.db.update(uname, enc_pass, service, select)
                     break
                 else:
@@ -273,6 +318,7 @@ class Helper:
         while True:
             try:
                 select = int(input("Enter choice of ID from before the User Names to Login: "))
+                print(select)
                 break
             except:
                 print("selection type for deletion not integer!!! ")
@@ -284,7 +330,7 @@ class Helper:
             creds = self.db.read_one(select)
             uname = creds[1]
             passw = creds[2]
-            dec_pass = self.sec.Decrypt(passw)
+            dec_pass = self.sec.Decrypt(passw, self.check_pass(), self.IV())
             service = creds[3]
             if service == "ggl":
                 self.lg.google(uname, dec_pass)
@@ -304,14 +350,12 @@ class Helper:
                 if passw1 == passw2:
                     hashed_pass = SHA256.new(passw1.encode('utf-8')).digest()
                     self.db.save_pass(hashed_pass)
-                    main()
                     break
                 else:
                     print("The Password's didn't match")
                     continue
-            except:
-                print("Error!!!")
-                continue
+            except Exception as e:
+                print(e)
 
     def update_pass(self):
         while True:
@@ -341,7 +385,7 @@ class Helper:
         while True:
             hash_obj = SHA256.new()
             cpassw = input('Enter Current Password: ')
-            hashed_cpassw = hash_obj.update(cpassw.encode('utf-8')).digest
+            hashed_cpassw = hash_obj.update(cpassw.encode('utf-8')).digest()
             retcpass = self.db.retrieve_pass()
             if hashed_cpassw == retcpass:
                 self.db.remove_pass()
@@ -351,109 +395,165 @@ class Helper:
                 continue
 
     def check_pass(self):
-        return self.db.retrieve_pass()[0]
+        try:
+            master_pass = self.db.retrieve_pass()[0]
+            return master_pass
+        except:
+            return None
 
+    def make_IV(self):
+        IV = self.sec.init_Vector()
+        self.save_iv(IV)
+
+    def save_iv(self, iv):
+        self.db.save_vector(iv)
+
+    def IV(self):
+        try:
+            IV = self.db.retrieve_vector()[0]
+            return IV
+        except:
+            return False
+
+    def update_iv(self):
+        iv = self.sec.init_Vector()
+        self.db.update_vector(iv)
+
+    def del_iv(self):
+        self.db.remove_vector()
+
+    def this_login(self):
+        userpass = input("Enter Login Password: ")
+        return userpass
+
+    def make_session(self, user_pass):
+        global _session
+        self.user_pass = user_pass
+        userhash = SHA256.new(self.user_pass.encode('utf-8')).digest()
+        stored_pass = self.check_pass()
+        if userhash == stored_pass:
+            _session = True
+            return True
+        else:
+            return False
+
+    def check_session(self):
+        global _session
+        if _session is True:
+            return True
+        else:
+            return False
 
 class Security:
 
-    global KEY, BLOCK_SIZE, IV, PAD, sec_obj
+    global BLOCK_SIZE
 
-
-    def Encrypt(self, passw, KEY):
-        self.passw = passw
+    def init_Vector(self):
+        global BLOCK_SIZE
         BLOCK_SIZE = 16
         IV = Random.new().read(BLOCK_SIZE)
-        PAD = "{"
-        sec_obj = AES.new(KEY, AES.MODE_CBC, IV=IV)
-        padding = lambda msg: msg + (BLOCK_SIZE - len(msg) % BLOCK_SIZE) * PAD
-        cipher = sec_obj.encrypt(padding(self.passw).encode('utf-8'))
-        return cipher
+        return IV
 
-    def Decrypt(self, passw):
+    def Encrypt(self, passw, KEY, IV):
+        BLOCK_SIZE = 16
         self.passw = passw
-        decipher = sec_obj.decrypt(self.passw)
-        unpad_init = decipher.find(PAD)
-        return decipher[:unpad_init]
+        PAD = "{"
+        cipher = AES.new(KEY, AES.MODE_CBC, IV=IV)
+        padding = lambda msg: msg + (BLOCK_SIZE - len(msg) % BLOCK_SIZE) * PAD
+        return cipher.encrypt(padding(self.passw).encode('utf-8'))
 
-
-
+    def Decrypt(self, passw, KEY, IV):
+        self.passw = passw
+        PAD = "{"
+        decipher = AES.new(KEY, AES.MODE_CBC, IV=IV)
+        plaintext = decipher.decrypt(self.passw).decode('utf-8')
+        pad_index = plaintext.find(PAD)
+        return plaintext[:pad_index]
 
 
 def main():
+
     sdict_select = int()
     sdict_res = str()
     bf = Helper()
     db = Db_methods()
     check = bf.check_pass()
+    IV = bf.IV()
+    if IV is False:
+        bf.make_IV()
+    else:
+        IV = bf.IV()
+        print(IV)
+
     if check is None:
         print("No Master Password is set. Please set a Master Password!!! ")
         bf.add_pass()
-
     else:
-
         while True:
-            userpass = input("Enter Login Password: ")
-            userhash = SHA256.new(userpass.encode('utf-8')).digest()
-            print(userhash)
-            stored_pass = check
-            print(stored_pass)
-            if userhash == stored_pass:
-                break
+            if bf.check_session() is False:
+                while True:
+                    login = bf.this_login()
+                    if bf.make_session(login) is True:
+                        break
+                    else:
+                        print("Wrong Password. Check and try again!!!")
+                        continue
+
             else:
-                print("Wrong Password. Check and try again!!!")
-                continue
+
+                print("""
+                    --> Choice of Options <--
+
+                    1 -> Display Accounts
+                    2 -> Add User Information
+                    3 -> Update User Information
+                    4 -> Delete User Information
+                    5 -> Login with Credentials
+
+                    """)
+
+                sdict = {
+                    1: bf.display,
+                    2: bf.add,
+                    3: bf.update,
+                    4: bf.delete,
+                    5: bf.login
+                }
+
+                while True:
+
+                    try:
+                        sdict_select = int(input("Enter choice of action: "))
+                        sdict_res = sdict.get(sdict_select, False)()
+                        break
+
+                    except:
+                        print("Invalid Selection!!! ")
+                        continue
 
 
-
-
-        print("""
-            --> Choice of Options <--
-
-            1 -> Display Accounts
-            2 -> Add User Information
-            3 -> Update User Information
-            4 -> Delete User Information
-            5 -> Login with Credentials
-
-            """)
-
-        sdict = {
-            1: bf.display,
-            2: bf.add,
-            3: bf.update,
-            4: bf.delete,
-            5: bf.login
-        }
-
-        while True:
-
-            try:
-                sdict_select = int(input("Enter choice of action: "))
-                sdict_res = sdict.get(sdict_select, False)()
-                break
-
-            except:
-                print("Invalid Selection!!! ")
-                continue
-
+def kill_session():
+    # while True:
+    #     print("started")
+    #     time.sleep(30)
+    #     global _session
+    #     _session = False
+    #     print("Session has expired. Please Login Again!!! ")
+    a = 1
 
 
 if __name__ == '__main__':
-    while True:
-        try:
-            main()
-        except KeyboardInterrupt:
-            print('Process is killed by keyboard interrupt')
-            time.sleep(5)
-            sys.exit(0)
 
+    main_thread = Thread(target=main)
+    main_thread.start()
+    s_kill = Thread(target=kill_session, daemon=True)
+    s_kill.start()
+    main_thread.join()
+    s_kill.join()
 
-
-
-
-
-
-#bf = Helper()
+# bf = Helper()
+# bf.make_IV()
+# print(bf.IV())
 
 # TEST RUN FOR INDIVIDUAL DATABASE FUNCTIONS #
 # db.save('whatever u', 'whatever p', 'ggl') # WORKS FINE #
